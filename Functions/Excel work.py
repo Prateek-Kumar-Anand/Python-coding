b92f80214d213ef0993a57381d0e518828c0ab91
Prateek-Kumar-Anand <prateekanand734@gmail.com>
2026-06-20 20:27:21 +0530

    updated work.

--- Functions/Excel work.py
+++ Functions/Excel work.py
@@ -1,17 +1,30 @@
 import openpyxl
+import pandas as pd
+import os
+
+
 print ("hellow user.")
+print('your current directry is ')
+a=os.getcwd()
+print(a)
+
+
 print ("please entre the path ")
 path = str(input())
+
 print ("entre the file name")
 name =str(input())
 file_path=path + "/" + name
 wb=openpyxl.load_workbook(file_path)
+
+print('your workbook name is ')
 print(wb)
-print("do you want to add a sheet")
-addingsheet=input()
-if addingsheet== 'yes':
-    wb.create_sheet(addingsheet)
-else :
-    print("okay sir")
-print
-("work done")
+
+print('CHOOSE SHEETS')
+s=wb.sheetnames()
+print(s)
+
+user_input=input()
+sheet=wb.get_sheet_by_name(user_input)
+S=sheet.title
+print(s)
